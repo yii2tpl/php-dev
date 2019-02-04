db1bc381b47456974769920c3869ec926694dc32
Vitaliy <theyamshikov@yandex.ru>
2019-02-04 10:27:35 +0600

    init

---
+++ console/cmd/main/generate app.bat
@@ -0,0 +1,5 @@
+@echo off
+cd ../../..
+cd vendor/yii2rails/yii2-app/bin
+php bin generate/app
+pause
---
+++ console/cmd/main/requirements.bat
@@ -0,0 +1,5 @@
+@echo off
+cd ../../..
+cd vendor/yii2rails/yii2-app/bin
+php bin requirements/all
+pause
